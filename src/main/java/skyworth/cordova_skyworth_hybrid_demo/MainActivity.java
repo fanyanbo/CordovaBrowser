da185dfc4a98e9a4563007f4de6e40a713a23fa8
fanyanbo <fanyanbo@skyworth.com>
2018-02-12 17:19:10 +0800

    添加了酷开系统ipc依赖 添加了cc_settings配置文件

--- src/main/java/skyworth/cordova_skyworth_hybrid_demo/MainActivity.java
+++ src/main/java/skyworth/cordova_skyworth_hybrid_demo/MainActivity.java
@@ -2,12 +2,71 @@ package skyworth.cordova_skyworth_hybrid_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.skyworth.framework.skysdk.ipc.SkyActivity;
+import com.tianci.system.api.TCSystemService;
+import com.tianci.system.data.TCInfoSetData;
+import com.tianci.system.data.TCSetData;
+import com.tianci.system.define.TCEnvKey;
+
+public class MainActivity extends SkyActivity {
+
+    private static final String mTag = "WebViewSDK";
+    private TCSystemService mSystemApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(mTag,"MainActivity onCreate");
+    }
+
+    @Override
+    public void onCmdConnectorInit() {
+        mSystemApi = new TCSystemService(this);
+        TCSetData modelSetData = mSystemApi
+                .getSetData(TCEnvKey.SKY_SYSTEM_ENV_MODEL);
+        String modelString = "";
+        if (modelSetData != null) {
+            TCInfoSetData modelInfoData = (TCInfoSetData) modelSetData;
+            modelString = modelInfoData.getCurrent();
+        }
+        Log.i(mTag,"MainActivity onCmdConnectorInit model = " + modelString);
+    }
+
+    @Override
+    public byte[] onHandler(String fromtarget, String cmd, byte[] body) {
+        return new byte[0];
+    }
+
+    @Override
+    public void onResult(String fromtarget, String cmd, byte[] body) {
+
+    }
+
+    @Override
+    public byte[] requestPause(String fromtarget, String cmd, byte[] body) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] requestResume(String fromtarget, String cmd, byte[] body) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] requestRelease(String fromtarget, String cmd, byte[] body) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] requestStartToVisible(String fromtarget, String cmd, byte[] body) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] requestStartToForground(String fromtarget, String cmd, byte[] body) {
+        return new byte[0];
     }
 }

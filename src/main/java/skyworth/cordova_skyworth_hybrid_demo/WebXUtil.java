0ee16edf7876a8cb73c96ae294a5b950e0526440
fanyanbo <fanyanbo@skyworth.com>
2018-03-07 15:37:25 +0800

    学习通过as增加文件

---
+++ src/main/java/skyworth/cordova_skyworth_hybrid_demo/WebXUtil.java
@@ -0,0 +1,19 @@
+package skyworth.cordova_skyworth_hybrid_demo;
+
+/**
+ * Created by fanyanbo on 2018/3/7.
+ * Email: fanyanbo@skyworth.com
+ */
+public class WebXUtil {
+
+    /**
+     * Add int.
+     *
+     * @param x the x
+     * @param y the y
+     * @return the int
+     */
+    int add(int x, int y) {
+        return x + y;
+    }
+}

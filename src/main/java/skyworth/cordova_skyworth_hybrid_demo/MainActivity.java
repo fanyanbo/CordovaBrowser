862190280e78adbc363f0eb8300a153de6fe3040
fanyanbo <fanyanbo@skyworth.com>
2018-08-09 09:18:50 +0800

    先同步代码

--- src/main/java/skyworth/cordova_skyworth_hybrid_demo/MainActivity.java
+++ src/main/java/skyworth/cordova_skyworth_hybrid_demo/MainActivity.java
@@ -1,9 +1,11 @@
 package skyworth.cordova_skyworth_hybrid_demo;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -17,7 +19,7 @@ import com.tianci.system.data.TCInfoSetData;
 import com.tianci.system.data.TCSetData;
 import com.tianci.system.define.TCEnvKey;
 
-public class MainActivity extends SkyActivity {
+public class MainActivity extends Activity {
 
     private static final String mTag = "WebViewSDK";
     private TCSystemService mSystemApi;
@@ -29,6 +31,17 @@ public class MainActivity extends SkyActivity {
     private EditText editText;
     private CheckBox cb;
     private boolean mIsChecked = false;
+    private boolean mIsHidden = false;
+    private int core = 0;
+    private Context mContext;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_1) {
+            core = (core == 0) ? 1 : 0;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +49,8 @@ public class MainActivity extends SkyActivity {
       //  setContentView(R.layout.activity_main);
         setContentView(R.layout.layout);
         Log.i(mTag,"MainActivity onCreate");
+
+        mContext = this;
 
         editText = (EditText)findViewById(R.id.content_url);
         cb = (CheckBox)findViewById(R.id.checkBox);
@@ -56,10 +71,11 @@ public class MainActivity extends SkyActivity {
                 Log.i(mTag,"onClick!!! url = " + urlcontent);
                 if( urlcontent != null && ( urlcontent.startsWith("http://") || urlcontent.startsWith("https://")))
                 {
-                    Intent mIntent = new Intent("com.coocaa.webview.test1");
+                    Intent mIntent = new Intent("com.coocaa.webview.test");
                     mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     mIntent.putExtra("url", urlcontent);
                     mIntent.putExtra("mode", 0);
+                    mIntent.putExtra("core", core);
                     mIntent.putExtra("cache", mIsChecked);
                     startActivity(mIntent);
                 }
@@ -72,6 +88,7 @@ public class MainActivity extends SkyActivity {
 
         btn1 = (Button)findViewById(R.id.button1);
         btn1.setText("启动活动页");
+        if (mIsHidden) btn1.setVisibility(View.INVISIBLE);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -83,7 +100,8 @@ public class MainActivity extends SkyActivity {
                     Intent mIntent = new Intent("com.coocaa.webview.test");
                     mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     mIntent.putExtra("url", urlcontent);
-                    mIntent.putExtra("mode", 1);
+                    mIntent.putExtra("mode", 0);
+                    mIntent.putExtra("core", core);
                     mIntent.putExtra("cache", mIsChecked);
                     startActivity(mIntent);
                 }
@@ -96,6 +114,7 @@ public class MainActivity extends SkyActivity {
 
         btn2 = (Button)findViewById(R.id.button2);
         btn2.setText("启动新本机信息");
+        if (mIsHidden) btn2.setVisibility(View.INVISIBLE);
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -108,6 +127,7 @@ public class MainActivity extends SkyActivity {
                     mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     mIntent.putExtra("url", urlcontent);
                     mIntent.putExtra("mode", 1);
+                    mIntent.putExtra("core", core);
                     mIntent.putExtra("cache", mIsChecked);
                     startActivity(mIntent);
                 }
@@ -120,6 +140,7 @@ public class MainActivity extends SkyActivity {
 
         btn3 = (Button)findViewById(R.id.button3);
         btn3.setText("启动江苏广电盒子引导页");
+        if (mIsHidden) btn3.setVisibility(View.INVISIBLE);
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -128,10 +149,11 @@ public class MainActivity extends SkyActivity {
                 Log.i(mTag,"onClick!!! url = " + urlcontent);
                 if( urlcontent != null && ( urlcontent.startsWith("http://") || urlcontent.startsWith("https://")))
                 {
-                    Intent mIntent = new Intent("com.coocaa.webview.test");
+                    Intent mIntent = new Intent("com.coocaa.webview.test2");
                     mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     mIntent.putExtra("url", urlcontent);
                     mIntent.putExtra("mode", 1);
+                    mIntent.putExtra("core", core);
                     mIntent.putExtra("cache", mIsChecked);
                     startActivity(mIntent);
                 }
@@ -144,6 +166,7 @@ public class MainActivity extends SkyActivity {
 
         btn4 = (Button)findViewById(R.id.button4);
         btn4.setText("启动长图宣传页");
+        if (mIsHidden) btn4.setVisibility(View.INVISIBLE);
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -152,12 +175,18 @@ public class MainActivity extends SkyActivity {
                 Log.i(mTag,"onClick!!! url = " + urlcontent);
                 if( urlcontent != null && ( urlcontent.startsWith("http://") || urlcontent.startsWith("https://")))
                 {
-                    Intent mIntent = new Intent("com.coocaa.webview.test");
-                    mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    mIntent.putExtra("url", urlcontent);
-                    mIntent.putExtra("mode", 1);
-                    mIntent.putExtra("cache", mIsChecked);
-                    startActivity(mIntent);
+//                    Intent mIntent = new Intent("com.coocaa.webview.test");
+//                    mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                    mIntent.putExtra("url", urlcontent);
+//                    mIntent.putExtra("mode", 1);
+//                    mIntent.putExtra("cache", mIsChecked);
+//                    startActivity(mIntent);
+
+//                    MainDialog.getInstance(mContext).show();
+
+//                    Intent serviceIntent = new Intent(mContext, MainService.class);
+//                    serviceIntent.putExtra("url","http://beta.webapp.skysrt.com/games/voice/index.html");
+//                    startService(serviceIntent);
                 }
                 else
                 {
@@ -165,53 +194,54 @@ public class MainActivity extends SkyActivity {
                 }
             }
         });
-    }
-
-    @Override
-    public void onCmdConnectorInit() {
-        mSystemApi = new TCSystemService(this);
-        TCSetData modelSetData = mSystemApi
-                .getSetData(TCEnvKey.SKY_SYSTEM_ENV_MODEL);
-        String modelString = "";
-        if (modelSetData != null) {
-            TCInfoSetData modelInfoData = (TCInfoSetData) modelSetData;
-            modelString = modelInfoData.getCurrent();
-        }
-        Log.i(mTag,"MainActivity onCmdConnectorInit model = " + modelString);
-    }
-
-    @Override
-    public byte[] onHandler(String fromtarget, String cmd, byte[] body) {
-        return new byte[0];
-    }
-
-    @Override
-    public void onResult(String fromtarget, String cmd, byte[] body) {
 
     }
 
-    @Override
-    public byte[] requestPause(String fromtarget, String cmd, byte[] body) {
-        return new byte[0];
-    }
-
-    @Override
-    public byte[] requestResume(String fromtarget, String cmd, byte[] body) {
-        return new byte[0];
-    }
-
-    @Override
-    public byte[] requestRelease(String fromtarget, String cmd, byte[] body) {
-        return new byte[0];
-    }
-
-    @Override
-    public byte[] requestStartToVisible(String fromtarget, String cmd, byte[] body) {
-        return new byte[0];
-    }
-
-    @Override
-    public byte[] requestStartToForground(String fromtarget, String cmd, byte[] body) {
-        return new byte[0];
-    }
+//    @Override
+//    public void onCmdConnectorInit() {
+//        mSystemApi = new TCSystemService(this);
+//        TCSetData modelSetData = mSystemApi
+//                .getSetData(TCEnvKey.SKY_SYSTEM_ENV_MODEL);
+//        String modelString = "";
+//        if (modelSetData != null) {
+//            TCInfoSetData modelInfoData = (TCInfoSetData) modelSetData;
+//            modelString = modelInfoData.getCurrent();
+//        }
+//        Log.i(mTag,"MainActivity onCmdConnectorInit model = " + modelString);
+//    }
+//
+//    @Override
+//    public byte[] onHandler(String fromtarget, String cmd, byte[] body) {
+//        return new byte[0];
+//    }
+//
+//    @Override
+//    public void onResult(String fromtarget, String cmd, byte[] body) {
+//
+//    }
+//
+//    @Override
+//    public byte[] requestPause(String fromtarget, String cmd, byte[] body) {
+//        return new byte[0];
+//    }
+//
+//    @Override
+//    public byte[] requestResume(String fromtarget, String cmd, byte[] body) {
+//        return new byte[0];
+//    }
+//
+//    @Override
+//    public byte[] requestRelease(String fromtarget, String cmd, byte[] body) {
+//        return new byte[0];
+//    }
+//
+//    @Override
+//    public byte[] requestStartToVisible(String fromtarget, String cmd, byte[] body) {
+//        return new byte[0];
+//    }
+//
+//    @Override
+//    public byte[] requestStartToForground(String fromtarget, String cmd, byte[] body) {
+//        return new byte[0];
+//    }
 }
--- src/main/java/skyworth/cordova_skyworth_hybrid_demo/WebViewActivity.java
+++ src/main/java/skyworth/cordova_skyworth_hybrid_demo/WebViewActivity.java
@@ -1,6 +1,7 @@
 package skyworth.cordova_skyworth_hybrid_demo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -14,27 +15,50 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class WebViewActivity extends CordovaExtActivity
-        implements CordovaExtActivity.CordovaWebViewListener, CordovaExtActivity.CordovaWebViewDataListener, CordovaExtActivity.CordovaErrorPageListener {
+        implements CordovaExtActivity.CordovaWebViewListener,  CordovaExtActivity.CordovaWebPageListener/*, CordovaExtActivity.CordovaBusinessDataListener*/ {
 
     public String url = null;
     public int mode = 0;
     public boolean isCache = false;
+    public int core = 0;
     private static final String mTag = "WebViewSDK";
+    public Timer timer;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Log.i("WebViewSDK","onKeyDown keyCode = " + keyCode);
+        return super.onKeyDown(keyCode, event);
+    }
 
     @Override
     public boolean dispatchKeyEvent(KeyEvent event) {
 
+     //   Log.i(mTag, "WebViewActivity dispatchKeyEvent keyCode = " + event.getKeyCode() + ",event = " + event.getAction() + ",tid = " + android.os.Process.myTid());
         if(event.getAction() == KeyEvent.ACTION_DOWN){
-            Log.i("WebViewSDK","keyCode = " + event.getKeyCode());
-            if(event.getKeyCode() == 8){
-                loadUrl("http://beta.webapp.skysrt.com/lxw/ceshi/nativeinfo2/index.html");
-            }else if(event.getKeyCode() == 9){
-                Map<String, String> map = new HashMap<String, String>();
-                map.put("action", "download");
-                map.put("params", "dfadsfgfsdgsaf");
-                CoocaaOSApi.broadCastCommonChanged(this,map);
+            Log.i("WebViewSDK","dispatchKeyEvent keyCode = " + event.getKeyCode());
+//            if(event.getKeyCode() == 10){
+//                loadUrl("http://172.20.139.227:3018/getcookie");
+//            }
+//            else if(event.getKeyCode() == 11){
+//                loadUrl("http://172.20.139.227:3018/setcookie");
+//            }
+//            else if(event.getKeyCode() == 12){
+//                loadUrl("http://beta.webapp.skysrt.com/games/cookie/cookie.html");
+//            }
+//            else if(event.getKeyCode() == 13){
+//                loadUrl("http://beta.webapp.skysrt.com/games/cookie/cookie2.html");
+//            }
+//            else if(event.getKeyCode() == 8){
+//                loadUrl("http://beta.webapp.skysrt.com/fyb/webapp/index.html");
+            if(event.getKeyCode() == 9){
+//                Map<String, String> map = new HashMap<String, String>();
+//                map.put("action", "download");
+//                map.put("params", "dfadsfgfsdgsaf");
+//                CoocaaOSApi.broadCastVoiceChanged(this,map);
             }
         }
         return super.dispatchKeyEvent(event);
@@ -43,36 +67,41 @@ public class WebViewActivity extends CordovaExtActivity
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
-        setCordovaWebViewListener(this);
-        setCordovaWebViewDataListener(this);
-        setCordovaErrorPageListener(this);
+        Log.i("WebViewSDK","====================================>onCreate");
 
+        setCordovaWebViewListener(this);
+     //   setCordovaWebViewDataListener(this);
+//        setCordovaBusinessDataListener(this);
+        setCordovaWebPageListener(this);
         LOG.setLogLevel(LOG.VERBOSE);
 
         setWebViewDisplayPolicy(1);
-      //  setCacheMode(1);
-
+        setCacheMode(0);
+//        setCore(0);
         super.onCreate(savedInstanceState);
-      //  setContentView(R.layout.activity_webview);
     }
 
     @Override
     public void onSuperCmdInit() {
         url = getIntent().getStringExtra("url");
         mode = getIntent().getIntExtra("mode", 0);
+        core = getIntent().getIntExtra("core", 0);
+        setCore(core);
         isCache = getIntent().getBooleanExtra("cache",false);
         if(isCache) setCacheMode(1);
-        Log.i(mTag, "WebViewActivity url:" + url + ",mode:" + mode + ",isCache:" + isCache);
+        Log.i(mTag, "WebViewActivity url:" + url + ",mode:" + mode + ",isCache:" + isCache + ",core:" + core);
 
-    //    url = "http://beta.webapp.skysrt.com/appstore/webxtest/test3/test.html";
-    //    url = "http://beta.webapp.skysrt.com/lxw/ceshi/nativeinfo2/index.html";
-   //     url = "http://beta.webapp.skysrt.com/lxw/ceshi/nfc2/index.html";
-  //      url = "http://beta.webapp.skysrt.com/appstore/ad/index2.html"; //audi广告
+   //     url = "https://webapp.skysrt.com/screen/index.html"; //监听demo
+   //     url = "http://beta.webapp.skysrt.com/fyb/screensaver/index.html";
+//        url = "http://beta.webapp.skysrt.com/fyb/study/index.html";
+      //  url = "http://beta.webapp.skysrt.com/fyb/vue/dist/index.html";
+//        url = "http://beta.webapp.skysrt.com/games/test/test.html";
+//          url = "http://beta.webapp.skysrt.com/games/webappdemo/index.html";
+//        url = "http://beta.webapp.skysrt.com/games/voice/index.html";
+//        url = "http://beta.webapp.skysrt.com/games/webappdemo/index.html";
+//        url = "http://beta.webapp.skysrt.com/fyb/canvas/2/index2.html";
 
-    //    url = "http://beta.webapp.skysrt.com/games/test/test.html"; //监听demo
-
-    //    url = "http://beta.webapp.skysrt.com/fyb/screensaver/index.html";
-
+        Log.i("WebViewSDK","================>ready to loadUrl url = " + url);
         if(mode == 0) {
             loadUrl(url);
         }else if(mode == 1) {
@@ -83,8 +112,18 @@ public class WebViewActivity extends CordovaExtActivity
     }
 
     @Override
-    public void onPageStarted(String url) {
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
 
+        String url = getIntent().getStringExtra("url");
+        int mode = getIntent().getIntExtra("mode",0);
+
+        Log.i("WebViewSDK","onNewIntent url = " + url + ",mode = " + mode);
+    }
+
+    @Override
+    public void onPageStarted(String url) {
+        Log.i("WebViewSDK","==================>onPageStarted");
     }
 
     @Override
@@ -94,8 +133,69 @@ public class WebViewActivity extends CordovaExtActivity
 
     @Override
     public void onPageError(int errorCode, String description, String failingUrl) {
-
+        Log.i("fyb","onPageError errorCode = " + errorCode + ",description = " + description + ",url = " + failingUrl);
     }
+
+
+//    @Override
+//    public String getBusinessData(String data) {
+//        Log.i("WebViewSDK","getBusinessData data = " + data);
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        return "aaa";
+//    }
+
+//    @Override
+//    public String getBusinessData(String data, final BussinessCallback cb) {
+////        try {
+////            Thread.sleep(5000);
+////        } catch (InterruptedException e) {
+////            e.printStackTrace();
+////        }
+////        cb.onResult("bbbbb");
+//        timer = new Timer();
+//        timer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                Log.i("WebViewSDK","getBusinessData timer----------->run");
+//                cb.onResult("ccccc");
+//            }
+//        }, 5000);
+//        Log.i("WebViewSDK","getBusinessData return----------->before");
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean setBusinessData(String data, final BussinessCallback cb) {
+//        Log.i("WebViewSDK","setBusinessData data = " + data);
+////        try {
+////            Thread.sleep(5000);
+////        } catch (InterruptedException e) {
+////            e.printStackTrace();
+////        }
+//
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                timer = new Timer();
+//                timer.schedule(new TimerTask() {
+//                    @Override
+//                    public void run() {
+//                        Log.i("WebViewSDK","setBusinessData timer----------->run");
+//                        cb.onResult("error1");
+//                        cb.onResult("error2");
+//                        cb.onResult("error3");
+//                        cb.onResult("success");
+//                    }
+//                }, 5000);
+//            }
+//        }).start();
+//
+//        return false;
+//    }
 
     @Override
     public void notifyMessage(String data) {
@@ -108,17 +208,18 @@ public class WebViewActivity extends CordovaExtActivity
     }
 
     @Override
-    public void notifyPageResume(String pageName, Map<String, String> map) {
+    public void notifyPageResume(String eventId, Map<String, String> map) {
 
     }
 
     @Override
-    public void notifyPagePause(String pageName) {
+    public void notifyPagePause(String eventId) {
 
     }
 
-    @Override
-    public void handleUI(String value) {
-
-    }
+//    @Override
+//    public void submitPromotionData(String headers, String params) {
+//        Log.i("TEST","WebViewActivity " + headers);
+//        Log.i("TEST","WebViewActivity " + params);
+//    }
 }
